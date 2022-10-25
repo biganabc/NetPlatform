038d50d6e1e81fb3efe8bb9e841739cdcb970aa3
邢云鹏 <19231177@buaa.edu.cn>
2022-10-25 18:21:06 +0800

    first

--- scripts/checkDNS.py
+++ scripts/checkDNS.py
@@ -1,5 +1,33 @@
 from scapy.all import *
 import os
+import time
+import json
+
+
+def get_self_ip():
+    s = requests.session()
+    s.keep_alive = False
+    try:
+        response = s.get("http://httpbin.org/get", timeout=10)
+        result = json.loads(response.text)
+    except Exception as ex:
+        return None
+    if "origin" not in result:
+        return None
+    else:
+        return result["origin"]
+
+
+def packet2str(packet):
+    bytes_of_packet = raw(packet)  # IP数据报的字节码序列
+    base64_of_packet = base64.b64encode(bytes_of_packet).decode()  # 直接得到的base64对象是bytes类型，首先解码为str类型
+    return base64_of_packet
+
+
+def str2packet(packet_str):
+    p = base64.b64decode(packet_str.encode())
+    ip_packet = IP(p)
+    return ip_packet
 
 
 def queryDNS(ip_dst, qname):
@@ -19,19 +47,24 @@ def queryDNS(ip_dst, qname):
     result = sr(p, verbose=0, timeout=2)
     if len(result[0]) == 0:
         print("无应答")
-        return None
+        return p, ""
     IP_ = result[0][0].answer["IP"]
     print("应答报文:")
     ls(IP_)
 
-    return IP_
+    return p, IP_
 
 
 if __name__ == "__main__":
-    os.system("dig baidu.com @114.114.114.114")
-    print("===" * 50)
-    os.system("dig baidu.com @8.8.8.8")
-    print("===" * 50)
-    os.system("dig baidu.com @39.156.66.10")
-    print("===" * 50)
-    os.system("dig baidu.com @106.11.172.9")
+    self_ip_str = get_self_ip()
+    if self_ip_str is None:
+        self_ip_str = "0.0.0.0"
+    send_packet, receive_packet = queryDNS("202.112.51.108",
+                                           self_ip_str + "." + str(int(time.time())) + ".queryrecord.com")
+    send_p_str = packet2str(send_packet)
+    if receive_packet != "":
+        receive_p_str = packet2str(receive_packet)
+    else:
+        receive_p_str = ""
+    with open("my_packets.json", "w") as f:
+        json.dump([send_p_str, receive_p_str], f)
